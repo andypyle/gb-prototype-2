802772c5ed67e831497cbdaa5c53618dd8c7a024
Andrew Pyle <andy.pyle@gmail.com>
2015-06-10 19:10:05 -0700

    Contact section done. Need to start adding sections for larger devices.

--- js/jq.js
+++ js/jq.js
@@ -1,14 +1,45 @@
 $(document).ready(function(){
 	var mobileToggle = $('.mob-nav');
 	var closeBigNews = $('.closeBigNews');
+	var navigation = $('.navigation');
+	var navLinks = $('.navigation li a');
+	//navigation.velocity({height: 'auto'});
+	var isNavVisible = false;
+
+	function reverseNav(n){
+		if(n)
+			n = !n;
+		else 
+			n = true;
+		return n;
+	}
 
 	mobileToggle.click(function(e){
 		e.preventDefault();
-		$('.navigation').stop().slideToggle();
+		
+		if(navigation.is(':visible'))
+			navigation.velocity('slideUp', {duration: 450, mobileHA: true});
+		else
+			navigation.velocity('slideDown');
 	});
+
+	navLinks.click(function(){
+		navigation.hide();
+	});
+
+
 
 	closeBigNews.click(function(e){
 		e.preventDefault();
-		$(this).parent('#bigNews').slideUp();
+		$(this)
+			.parent('#bigNews')
+				.velocity({
+					translateZ: 0, 
+					opacity: 0,
+					height: 0
+				}, {
+					duration: 750,
+					delay: 150
+				});
 	});
 });
